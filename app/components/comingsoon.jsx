340a9ed8808f174608799e259192ef32876fe546
saad <saadd@student.iul.ac.in>
2023-10-20 00:16:10 +0530

    coming soon added  to main

---
+++ app/components/comingsoon.jsx
@@ -0,0 +1,14 @@
+// pages/index.js
+
+import React from "react";
+import styles from "./comingsoon.module.css";
+
+const ComingSoon = () => {
+  return (
+    <div className={styles.comingSoonContainer}>
+      <h1 className={styles.comingSoonText}>Coming Soon</h1>
+    </div>
+  );
+};
+
+export default ComingSoon;
